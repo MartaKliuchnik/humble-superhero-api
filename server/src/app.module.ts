7c84427271a8f75dd7d68e964d59d93d5c22dedf
Marta <martasagun95@gmail.com>
2025-02-01 19:03:05 +0100

    feat: add middleware support and configure environment variables

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SuperheroesModule } from './superheroes/superheroes.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), SuperheroesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix('api/v1');
+
+  // Enable validation with whitelisting and forbidding non-whitelisted properties
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const port = configService.get<number>('APP_PORT') || 4000;
+  await app.listen(port);
 }
+
 bootstrap();
